5464b529ba1c54e81ddb37bab15b3d3719f317ad
unknown <Usuario@DESKTOP-O1Q957C.Tecnored.local>
2022-11-16 14:32:40 -0300

    adding scrolldown fix bug

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
 const express = require('express');
+const { scrollPageToBottom } = require('puppeteer-autoscroll-down')
 
 const app= express()
+
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
+
 
 app.get('/results',async(req,res)=>{
     
@@ -37,29 +43,41 @@ app.get('/results',async(req,res)=>{
         await page.click('body > prodeca-root > div > prodeca-toolbar > div > div > div:nth-child(5) > span');
         await page.waitForNavigation({ waitUntil: 'domcontentloaded' })
         await page.waitForSelector('body > prodeca-root > div > div > prodeca-friend-tournaments > div > div > div.col-12.col-md-8.col-lg-9 > div > prodeca-thumb-friend-tournament > div > div')
-        await page.click('body > prodeca-root > div > div > prodeca-friend-tournaments > div > div > div.col-12.col-md-8.col-lg-9 > div > prodeca-thumb-friend-tournament > div > div');
-        await page.waitForSelector('body > prodeca-root > div > div > prodeca-friend-tournament > div.container-fluid > div > div.col-12.col-md-8.col-lg-7 > prodeca-user-ranking:nth-child(1) > mat-card')
-    
+        await page.click('body > prodeca-root > div > div > prodeca-friend-tournaments > div > div > div.col-12.col-md-8.col-lg-9 > div > prodeca-thumb-friend-tournament > div > div');  
+        const lastPosition= await scrollPageToBottom(page, {
+            size: 110,
+            delay: 250,
+        })
+        await page.waitForSelector('body > prodeca-root > div > div > prodeca-friend-tournament > div.container-fluid > div > div.col-12.col-md-8.col-lg-7 > prodeca-user-ranking:nth-child(19) > mat-card')
 
         const data = await page.evaluate(() => {
             let result = []
-            for(let i=1; i<16;i++){
+            for(let i=1; i<19;i++){
                 const div = document.querySelector(`body > prodeca-root > div > div > prodeca-friend-tournament > div.container-fluid > div > div.col-12.col-md-8.col-lg-7 > prodeca-user-ranking:nth-child(${i})`).innerHTML;
                 result.push(div);
             }
             return result
         });
+        // close everything
+        await browser.close();
 
         return data
 
-        // close everything
-        // await page.close();
-        // await browser.close();
-
     }
+
+const regex=  /\>(.*?)\</g
 const results =await run();
-const jsondata= JSON.stringify(results)
-res.send(jsondata)
+const proceessresult=[]
+results.forEach((data)=>{
+    let value = [...data.match(regex)]
+    let values= [value[2][1],value[5],value[7][1]]
+    proceessresult.push(values)
+})
+
+
+console.log(proceessresult)
+res.send(proceessresult)
+
 
 })
 
